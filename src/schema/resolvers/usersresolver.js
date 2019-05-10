c3bbba23521b11caa64d002bd608eee445ed44d0
Matias Nunez <matydn05@gmail.com>
2019-05-10 08:30:58 -0300

    Create mutation

--- src/schema/resolvers/usersresolver.js
+++ src/schema/resolvers/usersresolver.js
@@ -4,6 +4,9 @@ const resolvers = {
   Query: {
     users: () => models.user.findAll(),
     user: (parent, { id }) => models.user.findOne({ where: { id } })
+  },
+  Mutation: {
+    createuser: (parent, args) => models.user.create(args, { fields: ['username', 'firstName', 'lastName', 'password'] })
   }
 }
 export default resolvers
